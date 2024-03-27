9f65389a00fc9638bd5f44fd82e83b21d5d6419a
Chandan Kamdi <dev.chandankamdi@gmail.com>
2024-03-27 20:14:21 +0530

    complete show user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -18,7 +18,7 @@ import {
   updateUserStart,
   updateUserSuccess,
 } from "../redux/user/userSlice.js";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -26,10 +26,10 @@ export default function Profile() {
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
-
   const [updateSuccess, setUpdateSuccess] = useState(false);
-
   const [formData, setFormData] = useState({});
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
 
   console.log(formData);
@@ -132,6 +132,22 @@ export default function Profile() {
       dispatch(signOutUserFailure(error.message));
     }
   };
+
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
   return (
     <div className=" p-3 max-w-lg mx-auto">
       <h1 className=" text-3xl font-semibold  text-center my-7">Profile</h1>
@@ -195,7 +211,10 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "update"}
         </button>
-        <Link className=" bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to="/create-listing">
+        <Link
+          className=" bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to="/create-listing"
+        >
           Create Listing
         </Link>
       </form>
@@ -214,6 +233,47 @@ export default function Profile() {
       <p className=" text-green-700 mt-5">
         {updateSuccess ? "User Details Updated Successfully" : ""}
       </p>
+      <button
+        type="button"
+        onClick={handleShowListings}
+        className=" text-green-700 w-full"
+      >
+        Show Listings
+      </button>
+      <p className=" text-red-700">
+        {showListingsError ? "Error showing listings" : ""}
+      </p>
+      {userListings && userListings.length > 0 && (
+        <div className=" flex flex-col gap-4">
+          <h1 className=" text-center mt-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className=" border p-3 rounded-lg flex justify-between items-center gap-4"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className=" h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className=" flex-1 text-slate-700 font-semibold hover:underline truncate"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className=" flex flex-col items-center">
+                <button className=" text-red-700 uppercase">Delete</button>
+                <button className=" text-green-700 uppercase">Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
